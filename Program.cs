eafd19423338862143bdf07ac094b1568049afa2
tbaraniuk <baraniuktaras098@gmail.com>
2024-12-27 15:31:20 +0200

    add files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.CommandLine;
+using github_activity.src;
+
+namespace GithubActivity
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var githubActivityOption = new Option<string>("--github-activity", "A github-activity for a user");
+
+            var rootCommand = new RootCommand("CLI app for getting user github activity info");
+            rootCommand.AddOption(githubActivityOption);
+
+            rootCommand.SetHandler(async (argument) => {
+                var result = await Api.GetUserActivity(argument);
+
+                Console.WriteLine(result);
+            }, githubActivityOption);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+    }
+}
---
+++ src/Api.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace github_activity.src
+{
+    public class Repo
+    {
+        public required string name {get; set;}
+    }
+
+    public class UserActivityApiResponse
+    {
+        public required string type {get; set;}
+        public required Repo repo {get; set;}
+        public int count {get; set;} = 1;
+    }
+
+    public class Api
+    {
+        public static async Task<string> GetUserActivity(string username)
+        {
+            try
+            {
+                var client = new HttpClient();
+
+                client.DefaultRequestHeaders.Add("User-Agent", "C# App");
+
+                var response = await client.GetAsync($"https://api.github.com/users/{username}/events");
+
+                response.EnsureSuccessStatusCode();
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                var userActivityApiResponse = JsonSerializer.Deserialize<UserActivityApiResponse[]>(jsonResponse);
+
+                if(userActivityApiResponse is null || userActivityApiResponse.Length == 0) {
+                    return "This user has no activity yet.\nPlease try another user";
+                }
+
+                var formattedList = new List<UserActivityApiResponse>();
+
+                for(var i = 0; i < userActivityApiResponse.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        formattedList.Add(userActivityApiResponse[i]);
+
+                        continue;
+                    }
+
+                    var lastElement = formattedList.Last();
+
+                    if((userActivityApiResponse[i].type == lastElement.type) && (userActivityApiResponse[i].repo.name == lastElement.repo.name))
+                    {
+                        formattedList.Last().count += 1;
+                    }
+                    else
+                    {
+                        formattedList.Add(userActivityApiResponse[i]);
+                    }
+                }
+
+                return GetUserActivityOutput(formattedList);
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+
+        private static string GetUserActivityOutput(List<UserActivityApiResponse> list)
+        {
+            string result = "";
+
+            foreach(UserActivityApiResponse item in list) {
+                switch(item.type)
+                {
+                    case "PushEvent":
+                        result += $"- Pushed {item.count} commit{(item.count == 1 ? "" : "s")} to {item.repo.name}\n";
+                        break;
+                    case "IssueCommentEvent":
+                        result += $"- Added issue {item.count} comment{(item.count == 1 ? "" : "s")} to {item.repo.name}\n";
+                        break;
+                    case "IssuesEvent":
+                        result += $"- Did {item.count} issue action{(item.count == 1 ? "" : "s")} to {item.repo.name}\n";
+                        break;
+                    case "PullRequestEvent":
+                        result += $"- Created {item.count} pull request{(item.count == 1 ? "" : "s")} to {item.repo.name}\n";
+                        break;
+                    case "CreateEvent":
+                        result += $"- Created {item.count} event{(item.count == 1 ? "" : "s")} to {item.repo.name}\n";
+                        break;
+                    case "WatchEvent":
+                        result += $"- Starred {item.repo.name}\n";
+                        break;
+                    default:
+                        result += $"- {item.type} {item.count} to {item.repo.name}\n";
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
